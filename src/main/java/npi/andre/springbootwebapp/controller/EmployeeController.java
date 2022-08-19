b749e2b2847edf31a31da7d35903604548bef0f1
André Sousa <andre.lipe.o@gmail.com>
2022-08-19 20:00:07 -0300

    Adiciona formulário e métodos para cadastrar um novo funcionário.

--- src/main/java/npi/andre/springbootwebapp/controller/EmployeeController.java
+++ src/main/java/npi/andre/springbootwebapp/controller/EmployeeController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import npi.andre.springbootwebapp.entity.Employee;
@@ -12,16 +14,30 @@ import npi.andre.springbootwebapp.repository.EmployeeRepository;
 
 @Controller
 public class EmployeeController {
-	
+
 	@Autowired
 	private EmployeeRepository employeeRepository;
-	
-	@GetMapping({"/", "/list", "/showEmployees"})
+
+	@GetMapping({ "/", "/list", "/showEmployees" })
 	public ModelAndView showEmployees() {
 		ModelAndView mav = new ModelAndView("list-employees");
 		List<Employee> list = employeeRepository.findAll();
 		mav.addObject("employees", list);
 		return mav;
+	}
+
+	@GetMapping("/addEmployeeForm")
+	public ModelAndView addEmployeeForm() {
+		ModelAndView mav = new ModelAndView("add-employee-form");
+		Employee newEmployee = new Employee();
+		mav.addObject("employee", newEmployee);
+		return mav;
+	}
+
+	@PostMapping("/saveEmployee")
+	public String saveEmployee(@ModelAttribute Employee employee) {
+		employeeRepository.save(employee);
+		return "redirect:/list";
 	}
 
 }
