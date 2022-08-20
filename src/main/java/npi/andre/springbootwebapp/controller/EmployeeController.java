b4bde27a6dcc5d6a153bffa04a4834bffebb2f8a
André Sousa <andre.lipe.o@gmail.com>
2022-08-20 13:45:55 -0300

    Adiciona função que chama o formulário de funcionários preenchido com dados de um funcionário já existente para atualização.

--- src/main/java/npi/andre/springbootwebapp/controller/EmployeeController.java
+++ src/main/java/npi/andre/springbootwebapp/controller/EmployeeController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import npi.andre.springbootwebapp.entity.Employee;
@@ -38,6 +39,14 @@ public class EmployeeController {
 	public String saveEmployee(@ModelAttribute Employee employee) {
 		employeeRepository.save(employee);
 		return "redirect:/list";
+	}
+
+	@GetMapping("/showUpdateForm")
+	public ModelAndView showUpdateForm(@RequestParam Long employeeId) {
+		ModelAndView mav = new ModelAndView("add-employee-form");
+		Employee employee = employeeRepository.findById(employeeId).get();
+		mav.addObject("employee", employee);
+		return mav;
 	}
 
 }
